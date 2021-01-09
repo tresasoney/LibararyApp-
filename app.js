7fe79323b5adafbb32e8d0e44cae160e1c9f4ec7
tresa <tresasoney2000@gmail.com>
2021-01-09 10:38:35 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const app = express();
+const port = process.env.PORT||5001
 
 
 const nav = [{
@@ -37,4 +38,4 @@ app.get('/',function(req,res){
 
 
 
-app.listen(50115);
+app.listen(port,()=>{console.log("Server Ready at"+port)});
